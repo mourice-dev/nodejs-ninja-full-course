81c0bb9edcd50385cacc0892477a455c60a73fc5
nshuti <nshutikope@gmail.com>
2026-06-13 17:54:52 +0200

    create server

---
+++ server.js
@@ -0,0 +1,9 @@
+const http = require("http");
+
+const server = http.createSever((req, res) => {
+    
+});
+
+server.listen(3000, "localhost", () => {
+console.log("server listining on port 3000")
+})
