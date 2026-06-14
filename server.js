f63c39193f1c82020841c2ff7f4f4cd201f9f838
nshuti <nshutikope@gmail.com>
2026-06-14 14:53:40 +0200

    fix: correct server creation and update listening port

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
-const http = require("http");
+/** @format */
 
-const server = http.createSever((req, res) => {
-    
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
+  res.setHeader("content-type", "text/html");
+  const index = fs.readFileSync("./index.html");
+  res.end(index);
 });
 
-server.listen(3000, "localhost", () => {
-console.log("server listining on port 3000")
-})
+server.listen(4000, "localhost", () => {
+  console.log("server listening on port 4000");
+});
