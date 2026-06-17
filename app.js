e137ec0cb1057be80ac2c0cecb970ad280bf3f75
nshuti <nshutikope@gmail.com>
2026-06-17 15:48:14 +0200

    feat: add logging middleware and update dependencies for morgan

--- app.js
+++ app.js
@@ -1,24 +1,65 @@
 /** @format */
 
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
+
 app.listen(3000);
 
 app.set("view engine", "ejs");
-app.use(express.static('public'));
+app.use(morgan("dev"));
+app.use(express.static("public"));
+
+app.use((req, res, next) => {
+  console.log(req.hostname);
+  console.log(req.url);
+  console.log(req.path);
+  console.log(req.method);
+  next();
+});
 
 const blogs = [
-    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-  ];
+  {
+    title: "Yoshi finds eggs",
+    snippet: "Lorem ipsum dolor sit amet consectetur",
+  },
+  {
+    title: "Mario finds stars",
+    snippet: "Lorem ipsum dolor sit amet consectetur",
+  },
+  {
+    title: "How to defeat bowser",
+    snippet: "Lorem ipsum dolor sit amet consectetur",
+  },
+  {
+    title: "Yoshi finds eggs",
+    snippet: "Lorem ipsum dolor sit amet consectetur",
+  },
+  {
+    title: "Mario finds stars",
+    snippet: "Lorem ipsum dolor sit amet consectetur",
+  },
+  {
+    title: "How to defeat bowser",
+    snippet: "Lorem ipsum dolor sit amet consectetur",
+  },
+  {
+    title: "Yoshi finds eggs",
+    snippet: "Lorem ipsum dolor sit amet consectetur",
+  },
+  {
+    title: "Mario finds stars",
+    snippet: "Lorem ipsum dolor sit amet consectetur",
+  },
+  {
+    title: "How to defeat bowser",
+    snippet: "Lorem ipsum dolor sit amet consectetur",
+  },
+  {
+    title: "How to defeat bowser",
+    snippet: "Lorem ipsum dolor sit amet consectetur",
+  },
+];
 
 app.get("/", (req, res) => {
   res.render("index", { title: "Home", blogs });
