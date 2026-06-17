68791ad6d3e9ed48c6345dd9bc6d3d60725f1a50
nshuti <nshutikope@gmail.com>
2026-06-17 15:36:36 +0200

    feat: restructure project with EJS views and remove unused HTML files

--- app.js
+++ app.js
@@ -2,17 +2,36 @@
 
 const express = require("express");
 const app = express();
+app.listen(3000);
 
-app.get("./", (req, res) => {
-  res.sendFile("index.html", { root: __dirname });
+app.set("view engine", "ejs");
+app.use(express.static('public'));
+
+const blogs = [
+    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  ];
+
+app.get("/", (req, res) => {
+  res.render("index", { title: "Home", blogs });
 });
-app.get("./about", (req, res) => {
-  res.sendFile("index.html", { root: __dirname });
+app.get("/about", (req, res) => {
+  res.render("about", { title: "About" });
 });
-app.get("./about-us", (req, res) => {
+app.get("/about-us", (req, res) => {
   res.redirect("/about");
 });
-app.use((req, res) => {
-  res.status(404).sendFile("404.html", { root: __dirname });
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create" });
 });
-app.listen(3000);
+app.use((req, res) => {
+  res.status(404).render("404", { title: "404" });
+});
